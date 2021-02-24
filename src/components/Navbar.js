c4356c3d240a0f1aa663563721ceb43b0fadb7bf
Ashadur Rahman <Ashadurcodes@gmail.com>
2021-02-24 12:21:43 +0000

    made navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav className="navbar">
       <div className="nav-center">
-        <Link to="/">
-          <h1 className="logo">MyMovies</h1>
-        </Link>
-        <ul className="nav-links">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
+        <div className="nav-header">
+          <Link to="/">
+            <h1 className="logo">MyMovies</h1>
+          </Link>
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
               <Link to="/mymovies">Watchlist</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
